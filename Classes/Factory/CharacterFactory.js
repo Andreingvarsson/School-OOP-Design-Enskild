afd25b78c18430f1dd572d4ce125dddc3430c7d1
André <andreingvarsson@hotmail.com>
2020-10-09 12:59:41 +0200

    Creating a characterFactory to create characters.

--- Classes/Factory/CharacterFactory.js
+++ Classes/Factory/CharacterFactory.js
@@ -1,7 +1,26 @@
+const Necromancer = require("../Characters/Necromancer");
+const Wizard = require("../Characters/Wizard");
+const Barbarian = require('../Characters/Barbarian');
+const Assasin = require('../Characters/Assasin');
+
 module.exports = class CharacterFactory {
 
     constructor(){
     }
 
 
+    static CreateNewCharacter(character){
+        
+        switch(character.class){
+            case 'Assasin': let as = new Assasin(character.name, character.race)
+            return as;
+            case 'Barbarian': let barb = new Barbarian(character.name, character.race)
+            return barb;
+            case 'Necromancer': let necro = new Necromancer(character.name, character.race)
+            return necro;
+            case 'Wizard': let wiz = new Wizard(character.name, character.race)
+            return wiz;
+        }
+    }
+    
 }
