44583f2a1327dc9fb4142f5525e3bb45c9de3fcf
André <andreingvarsson@hotmail.com>
2020-10-10 11:36:59 +0200

    AttackDecorator giving classes attacks when created.

--- Classes/Factory/CharacterFactory.js
+++ Classes/Factory/CharacterFactory.js
@@ -2,28 +2,37 @@ const Necromancer = require("../Characters/Necromancer");
 const Wizard = require("../Characters/Wizard");
 const Barbarian = require("../Characters/Barbarian");
 const Assasin = require("../Characters/Assasin");
-const SpecialMoveAdapter = require("../Adaptor/SpecialMoveAdapter");
+const ClassMessageAdapter = require("../Adaptor/ClassMessageAdapter");
+const AttackDecoractor = require("../decorator/AttackDecorator");
 
 module.exports = class CharacterFactory {
   constructor() {}
 
-  static CreateNewCharacter(character) {
+  static createNewCharacter(character) {
     switch (character.class) {
       case "Assasin":
         let as = new Assasin(character.name, character.race);
-        console.log(`\n${as.name} ` + SpecialMoveAdapter.saySomething(as));
+        console.log(`\n${as.name} ` + ClassMessageAdapter.saySomething(as));
+        AttackDecoractor.teachAttacks(as);
+        console.log(as);
         return as;
       case "Barbarian":
         let barb = new Barbarian(character.name, character.race);
-        console.log(`\n${barb.name} ` + SpecialMoveAdapter.saySomething(barb));
+        console.log(`\n${barb.name} ` + ClassMessageAdapter.saySomething(barb));
+        AttackDecoractor.teachAttacks(barb);
+        console.log(barb);
         return barb;
       case "Necromancer":
         let necro = new Necromancer(character.name, character.race);
-        console.log(`\n${necro.name} ` + SpecialMoveAdapter.saySomething(necro));
+        console.log(`\n${necro.name} ` + ClassMessageAdapter.saySomething(necro));
+        AttackDecoractor.teachAttacks(necro);
+        console.log(necro);
         return necro;
       case "Wizard":
         let wiz = new Wizard(character.name, character.race);
-        console.log(`\n${wiz.name} ` + SpecialMoveAdapter.saySomething(wiz));
+        console.log(`\n${wiz.name} ` + ClassMessageAdapter.saySomething(wiz));
+        AttackDecoractor.teachAttacks(wiz);
+        console.log(wiz);
         return wiz;
     }
   }
--- Classes/decorator/AttackDecorator.js
+++ Classes/decorator/AttackDecorator.js
@@ -1,45 +1,50 @@
 module.exports = class AttackDecorator {
+  // Välja helt själv eller ska jag overrida och ge attacker??
 
-    
-// Välja helt själv eller ska jag overrida och ge attacker??
+  // ta bort damage då jag inte använder det?? 
 
-    necromancerAtk = [
-        { name: "Poison nova", damage: 10 },
-        { name: "Corpse explosion", damage: 15 },
-        { name: "Bone spear", damage: 20 },
-      ];
+  static necromancerAtk = [
+    { name: "Poison nova", damage: 10 },
+    { name: "Corpse explosion", damage: 15 },
+    { name: "Bone spear", damage: 20 },
+  ];
 
-      wizardAtk = [
-        { name: "Frost bolt", damage: 10 },
-        { name: "Fire blast", damage: 15 },
-        { name: "Pyroblast", damage: 20 },
-      ];
+  static wizardAtk = [
+    { name: "Frost bolt", damage: 10 },
+    { name: "Fire blast", damage: 15 },
+    { name: "Pyroblast", damage: 20 },
+  ];
 
-      barbarianAtk = [
-        { name: "Bash", damage: 5 },
-        { name: "Frenzy", damage: 10 },
-        { name: "Whirlwind", damage: 15 },
-      ];
+  static barbarianAtk = [
+    { name: "Bash", damage: 5 },
+    { name: "Frenzy", damage: 10 },
+    { name: "Whirlwind", damage: 15 },
+  ];
 
-      assasinAtk = [
-        { name: "Jab", damage: 5 },
-        { name: "Poison", damage: 10 },
-        { name: "Impale", damage: 15 },
-      ];
+  static assasinAtk = [
+    { name: "Jab", damage: 5 },
+    { name: "Poison", damage: 10 },
+    { name: "Impale", damage: 15 },
+  ];
 
+  constructor() {
+    throw new Error("AttackDecorator is a static class");
+  }
 
-
-    constructor(){
-        throw new Error('AttackDecorator is a static class')
+  static teachAttacks(object) {
+    if (!object.class) {
+      throw new Error("You need a specific class to  learn attacks");
     }
 
-    static teachAttacks(object){
-        if(!object.class){
-            throw new Error('You need a specific class to  learn attacks')
-        }
-
-        // switch?
+    switch (object.class) {
+      case "Assasin":
+        return (object.assasinAtk = this.assasinAtk);
+      case "Barbarian":
+        return (object.barbarianAtk = this.barbarianAtk);
+      case "Wizard":
+        return (object.wizardAtk = this.wizardAtk);
+      case "Necromancer":
+        return (object.necromancerAtk = this.wizardAtk);
     }
-
-
-}
+  }
+};
